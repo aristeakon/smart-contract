dee1d06ed483a33a24be40a5f43573fea78fc223
aristeakon <aristeakontogianni@gmail.com>
2024-05-18 13:01:22 +0300

    Add files via upload

---
+++ Counter.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Counter{
+    
+    // unsigned integer (no sign such as "-" )
+    // it's a data type that represent positive values
+    uint public count;
+    // get the current count
+    // "public" visibility, "view" function type, returns "uint"
+    function get() public view returns (uint) {
+    return count;
+    }
+    // increment count by 1
+    function inc() public {
+    count += 1;
+    }
+    // decrement count by 1
+    function dec() public {
+    // it will fail if count = 0
+    count -= 1;
+    }
+}
+
