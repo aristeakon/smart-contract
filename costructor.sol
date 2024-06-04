808d7bfe6f20ae3889b81e23270335d0bda04c17
aristeakon <aristeakontogianni@gmail.com>
2024-06-04 13:14:11 +0300

    Update costructor.sol

--- costructor.sol
+++ costructor.sol
@@ -1,1 +1,23 @@
+pragma solidity ^0.8.24;
 
+contract MyContract {
+  // State variables
+  uint256 public balance;
+
+  // Constructor
+  constructor() {
+    // Initialize balance to 0
+    balance = 0;
+  }
+
+  // Function to deposit funds
+  function deposit(uint256 amount) public {
+    balance += amount;
+  }
+
+  // Function to withdraw funds
+  function withdraw(uint256 amount) public {
+    require(balance >= amount, "Insufficient balance");
+    balance -= amount;
+  }
+}
