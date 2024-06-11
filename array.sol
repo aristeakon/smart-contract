093f248e3231c7057f53105349fa58a22695ff35
aristeakon <aristeakontogianni@gmail.com>
2024-06-11 14:25:50 +0300

    Update array.sol

--- array.sol
+++ array.sol
@@ -1,1 +1,21 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ArrayExample {
+    // Στατικό array
+    uint[5] public fixedArray;
+
+    // Δυναμικό array
+    uint[] public dynamicArray;
+
+    // Συνάρτηση για την προσθήκη τιμής στο δυναμικό array
+    function addValue(uint _value) public {
+        dynamicArray.push(_value);
+    }
+
+    // Συνάρτηση για την ανάκτηση του μήκους του δυναμικού array
+    function getArrayLength() public view returns (uint) {
+        return dynamicArray.length;
+    }
+}
