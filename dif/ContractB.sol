00c969eafdbcc3c47e38367717c7818b738c1997
aristeakon <aristeakontogianni@gmail.com>
2024-06-11 13:18:04 +0300

    Update ContractB.sol

--- dif/ContractB.sol
+++ dif/ContractB.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ContractB {
+    uint public value;
+
+    constructor(uint _value) {
+        value = _value;
+    }
+
+    function getValue() public view returns (uint) {
+        return value;
+    }
+}
 
