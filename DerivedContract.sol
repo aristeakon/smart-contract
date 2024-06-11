ab4530df7a06730a1c2b5fd766950df0b5cf557d
aristeakon <aristeakontogianni@gmail.com>
2024-06-11 12:15:32 +0300

    Update DerivedContract.sol

--- DerivedContract.sol
+++ DerivedContract.sol
@@ -1,1 +1,23 @@
+// Δήλωση του παραγόμενου συμβολαίου που κληρονομεί από το BaseContract
+contract DerivedContract is BaseContract {
+    // Δημόσια μεταβλητή για αποθήκευση του παραγόμενου μηνύματος
+    string public derivedMessage;
 
+    // Κατασκευαστής του παραγόμενου συμβολαίου
+    constructor() {
+        // Υπερισχύει το βασικό μήνυμα
+        baseMessage = "Overridden Base Message";
+        // Αρχικοποιεί το παραγόμενο μήνυμα
+        derivedMessage = "Hello from DerivedContract";
+    }
+
+    // Συνάρτηση για τον καθορισμό του παραγόμενου μηνύματος
+    function setDerivedMessage(string memory newMessage) public {
+        derivedMessage = newMessage;
+    }
+
+    // Συνάρτηση για την ανάκτηση του παραγόμενου μηνύματος
+    function getDerivedMessage() public view returns (string memory) {
+        return derivedMessage;
+    }
+}
