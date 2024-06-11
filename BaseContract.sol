c5330f34a5f534ae2c2a8e449a9e34cc5c4318a4
aristeakon <aristeakontogianni@gmail.com>
2024-06-11 12:14:42 +0300

    Update BaseContract.sol

--- BaseContract.sol
+++ BaseContract.sol
@@ -1,17 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Δήλωση του βασικού συμβολαίου
 contract BaseContract {
+    // Δημόσια μεταβλητή για αποθήκευση μηνύματος
     string public baseMessage;
 
+    // Κατασκευαστής του βασικού συμβολαίου που αρχικοποιεί το μήνυμα
     constructor() {
         baseMessage = "Hello from BaseContract";
     }
 
+    // Συνάρτηση για τον καθορισμό του βασικού μηνύματος
     function setBaseMessage(string memory newMessage) public {
         baseMessage = newMessage;
     }
 
+    // Συνάρτηση για την ανάκτηση του βασικού μηνύματος
     function getBaseMessage() public view returns (string memory) {
         return baseMessage;
     }
