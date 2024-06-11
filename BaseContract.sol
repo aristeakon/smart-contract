66681d071338a5a6b76cfff7c45cf77421170e15
aristeakon <aristeakontogianni@gmail.com>
2024-06-11 12:12:54 +0300

    Update BaseContract.sol

--- BaseContract.sol
+++ BaseContract.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract BaseContract {
+    string public baseMessage;
+
+    constructor() {
+        baseMessage = "Hello from BaseContract";
+    }
+
+    function setBaseMessage(string memory newMessage) public {
+        baseMessage = newMessage;
+    }
+
+    function getBaseMessage() public view returns (string memory) {
+        return baseMessage;
+    }
+}
